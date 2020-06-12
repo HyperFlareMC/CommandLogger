d8862a11f8e721d9914a487c70a55920c2be90ab
HyperFlareMC <oncespokenmc@gmail.com>
2020-06-12 15:58:14 -0600

    Initial

---
+++ src/HyperFlareMC/CommandLogger/Main.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HyperFlareMC\CommandLogger;
+
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable() : void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerCommandPreprocess(PlayerCommandPreprocessEvent $event){
+        $player = $event->getPlayer();
+        $console = new ConsoleCommandSender();
+        $message = $event->getMessage();
+        if($message[0] === "/"){
+            $console->sendMessage(TextFormat::AQUA . "<" . $player->getName() . "> [CMD] " . TextFormat::clean($message));
+        }
+    }
+
+}
