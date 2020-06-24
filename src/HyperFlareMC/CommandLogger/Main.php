3b5fcd0b8f291ff93181db56ae72dba82bdf60b1
HyperFlareMC <oncespokenmc@gmail.com>
2020-06-23 23:18:24 -0600

    Added configuration options

--- src/HyperFlareMC/CommandLogger/Main.php
+++ src/HyperFlareMC/CommandLogger/Main.php
@@ -8,12 +8,31 @@ use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerCommandPreprocessEvent;
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
 use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
 
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * @var string
+     */
+    private $loggerMessage;
+
+    /**
+     * @var string
+     */
+    private $messageContents;
+
     public function onEnable() : void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+        $this->config = $this->getConfig();
+        $this->loggerMessage = $this->config->get("logger-message");
     }
 
     public function onPlayerCommandPreprocess(PlayerCommandPreprocessEvent $event){
@@ -21,7 +40,15 @@ class Main extends PluginBase implements Listener{
         $console = new ConsoleCommandSender();
         $message = $event->getMessage();
         if($message[0] === "/"){
-            $console->sendMessage(TextFormat::WHITE . "COMMAND: <" . $player->getName() . "> " . TextFormat::clean($message));
+            $command = $this->loggerMessage;
+            $replacements = [
+                "{command_name}" => $message,
+                "{player_name}" => $player->getName()
+            ];
+            foreach($replacements as $tag => $def){
+                $command = str_replace($tag, $def, $command);
+            }
+            $console->sendMessage($command);
         }
     }
 }
